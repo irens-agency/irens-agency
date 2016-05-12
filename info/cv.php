2cd60c14f7fc6d104be8c7549f8d2297b6f715bd
irens <rootdevirens@gmail.com>
2016-05-12 19:31:29 +0300

    fixed grammar error

--- info/cv.php
+++ info/cv.php
@@ -1,4 +1,4 @@
-<?php 
-    mail("irensagency@mail.ru", "Резюме", "Имя Фамилия:\n$_POST[name]\n\nОпыт работы:\n$_POST[experience]\n\nПрофессиональные навыки:\n$_POST[skills]\n\nУровень английского языка:\n$_POST[english]\n\nДополнительная ифнормация:\n$_POST[info]\n\nТелефон:$_POST[phone]\n\nSkype:$_POST[skype]");
+<?php
+    mail("irensagency@mail.ru", "Резюме", "Имя Фамилия:\n$_POST[name]\n\nДолжность:\n$_POST[prof]\n\nОпыт работы:\n$_POST[experience]\n\nПрофессиональные навыки:\n$_POST[skills]\n\nУровень английского языка:\n$_POST[english]\n\nДополнительная ифнормация:\n$_POST[info]\n\nТелефон:$_POST[phone]\n\nSkype:$_POST[skype]");
     header('Location: thanks.html');
 ?>
