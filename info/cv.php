f7e46ee0bd5549f1242fb238ca4643d2a4af3076
MaxNeutrino <max.stepovoy@gmail.com>
2016-09-06 15:57:43 +0300

    New functions

--- info/cv.php
+++ info/cv.php
@@ -1,4 +1,6 @@
 <?php
-    mail("irensagency@mail.ru", "Резюме", "Имя Фамилия:\n$_POST[name]\n\nДолжность:\n$_POST[prof]\n\nОпыт работы:\n$_POST[experience]\n\nПрофессиональные навыки:\n$_POST[skills]\n\nУровень английского языка:\n$_POST[english]\n\nДополнительная ифнормация:\n$_POST[info]\n\nТелефон:$_POST[phone]\n\nSkype:$_POST[skype]");
+    $header = "From: \"Резюме IRENS\" <irens.com.ua>\n";
+    $header .= "Content-type: text/plain; charset=\"utf-8\"";
+    mail("irensagency@mail.ru", "Резюме", "Имя Фамилия:\n$_POST[name]\n\nДолжность:\n$_POST[prof]\n\nОпыт работы:\n$_POST[experience]\n\nПрофессиональные навыки:\n$_POST[skills]\n\nУровень английского языка:\n$_POST[english]\n\nДополнительная ифнормация:\n$_POST[info]\n\nТелефон:$_POST[phone]\n\nSkype:$_POST[skype]", $header);
     header('Location: thanks.html');
 ?>
--- info/send.php
+++ info/send.php
@@ -1,4 +1,6 @@
-<?php 
-    mail("irensagency@mail.ru", "Обратная связь", "Имя: $_POST[name] \nТелефон: $_POST[phone]");
-    header('Location: thanks.html#thanks');
+<?php
+    $header = "From: \"Обратная связь IRENS\" <irens.com.ua>\n";
+    $header .= "Content-type: text/plain; charset=\"utf-8\"";
+    mail("irensagency@mail.ru", "Обратная связь", "Имя: $_POST[name] \nТелефон: $_POST[phone] \nСоциальные сети: $_POST[network]", $header);
+    header('Location: thankyou.html#thanks');
 ?>
